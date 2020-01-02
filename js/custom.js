08d562fff65b0f5f9a9f87bdd514ec7f5f667056
marnixnixnix <marnixklapwijk@hotmail.com>
2020-01-02 17:28:42 +0100

    Het grootste deel van de front-end gemaakt

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$('.datepicker').pickadate({
+weekdaysShort: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+showMonthsShort: true
+})
+// Time Picker Initialization
+$('#input_starttime').pickatime({});
