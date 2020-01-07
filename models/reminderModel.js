cd0853f140f294afc1a94d6db6476c8550d6fa19
unknown <tylerbroere@gmail.com>
2020-01-07 15:17:00 +0100

    finish reminderModel.js

--- models/reminderModel.js
+++ models/reminderModel.js
@@ -8,88 +8,30 @@ const Schema = mongoose.Schema;
 // NEW REFERENCE/ID
 //	_id: new mongoose.Types.ObjectId(),
 
-const userSchema = new Schema({
-	voornaam: {
+const reminderSchema = new Schema({
+	title: {
 		type: String,
 		required: true,
-		minlength: 1,
-		maxlength: 50
+		minLength: 2,
+		maxLength: 30
 	},
-	achternaam: {
+	message: {
 		type: String,
 		required: true,
-		minlength: 1,
-		maxlength: 70
+		minLength: 1,
 	},
-	full_name: {
-		type: String,
-		default: function () {
-			return this.voornaam + " " + this.achternaam;
-		}
-	},
-	email: {
-		type: String,
+	remind_date: {
+		type: Date,
 		required: true,
-		unique: true
+		trim: true
 	},
-	wachtwoord: {
-		type: String,
-		required: true,
-		minlength: 6,
-		maxlength: 255
-	},
-	img: {
-		data: Buffer,
-		contentType: String,
-	},
-	src: {
-		type: String,
-		default: function () {
-			let newBuffer = {
-				data: new Uint8Array(this.img.data.buffer),
-				contentType: this.img.contentType
-			}
-			//  Converts to base64 (for html rendering)
-			let newBase = new Buffer(newBuffer.data).toString('base64');
-			imgSource = `data:${newBuffer.contentType};base64,${newBase}`; //! Variable for img src=""
-
-			//console.log(imgSource);
-			return imgSource;
-		}
-	},
-	biografie: {
-		type: String,
-		default: "",
-		minlength: 0,
-		maxlength: 255
-	},
-	tijdlijn: {
-		type: Schema.Types.ObjectId,
-		ref: 'tijdlijn'
-	},
-	following: [{
+	user: {
 		type: Schema.Types.ObjectId,
 		ref: 'user'
-	}],
-	followers: [{
-		type: Schema.Types.ObjectId,
-		ref: 'user'
-	}]
+	},
 });
 
-//	Doesn't work for some reason
-userSchema.methods.generateAuthToken = function () {
-	const token = jwt.sign({
-		email: user[0].email,
-		userId: user[0]._id,
-		//isAdmin: true,
-	}, jwtPrivateKey, {
-		expiresIn: "1h"
-	});
-	return token;
-};
-
-const userModel = mongoose.model('user', userSchema);
+const reminderModel = mongoose.model('reminder', reminderSchema);
 
 
 function validateInput(validation_type, input) {
@@ -98,70 +40,21 @@ function validateInput(validation_type, input) {
 
 	switch (validation_type) {
 
-		case 'login':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-				email: Joi.string().email().required(),
-				wachtwoord: Joi.string().required(),
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
 
-		case 'register':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-				voornaam: Joi.string().min(1).max(50).required(),
-				achternaam: Joi.string().min(1).max(70).required(),
-				email: Joi.string().email().required(),
-				wachtwoord: Joi.string().min(6).max(255).required(),
-				wachtwoord_herhalen: Joi.string().min(6).max(255).valid(Joi.ref('wachtwoord')).required(),
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
+		schema = Joi.object({
+			title: Joi.string().min(2).max(30).required(),
+			message: Joi.string().min(2).required(),
+			remind_date: Joi.date().required()
+		});
 
-		case 'set_avatar':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-				data: Joi.binary().encoding('base64').required(),
-				contentType: Joi.required()
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
+		//	Create
+		//	Update
+		//	Delete
+		case '':
+		break;
 
-		case 'set_bio':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-				biografie: Joi.string().min(0).max(255).allow('').required()
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
-
-		case 'set_userinfo':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-				voornaam: Joi.string().min(1).max(50).required(),
-				achternaam: Joi.string().min(1).max(70).required(),
-				email: Joi.string().email().required(),
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
-
-		case 'set_password':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-				wachtwoord: Joi.string().min(6).max(255).required(),
-				wachtwoord_herhalen: Joi.string().min(6).max(255).valid(Joi.ref('wachtwoord')).required(),
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
-
-		case 'upload_image':
-			console.log(`Validation type: ${validation_type}`);
-			schema = Joi.object({
-
-			});
-			return schema.validate(input, (error, value) => {});
-			break;
+	
+		return schema.validate(input, (error, value) => {});
 
 		default:
 			console.log('ERROR: validation request type not valid');
@@ -173,5 +66,5 @@ function validateInput(validation_type, input) {
 
 }
 
-module.exports.User = userModel;
+module.exports.User = reminderModel;
 module.exports.validateInput = validateInput;
